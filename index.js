15c2c42519cc4e6b581cb15d4d699d77649aa913
Tembot <tixon_337@MacBook-Pro-Tembot.local>
2021-10-18 00:09:58 +0300

    done

---
+++ index.js
@@ -0,0 +1,41 @@
+// Подключаем библиотеку для работы с Telegram API в переменную
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Устанавливаем токен, который выдавал нам бот
+const token = '2044126598:AAGVdD2LT_Rd1B5joOYA2EfmFqgSW5uOnpQ';
+const happyDate = new Date('2021-07-16 21:00');
+const bot = new TelegramBot(token, { polling: true });
+
+function diffDates(day_one, day_two) {
+	return Math.round((day_one - day_two) / (60 * 60 * 24 * 1000));
+}
+
+function diffHours(day_one, day_two) {
+	return Math.round((day_one - day_two) / (60 * 60 * 1000));
+}
+
+function diffMinutes(day_one, day_two) {
+	return Math.round((day_one - day_two) / (60 * 1000));
+}
+
+function diffSeconds(day_one, day_two) {
+	return Math.round((day_one - day_two) / 1000);
+}
+
+bot.onText(/сколько/gi, (msg, match) => {
+	const fromId = msg.from.id; // Получаем ID отправителя
+	const nowDate = new Date();
+	console.log(msg.from);
+	bot.sendMessage(fromId, `Привет ${msg.from.username}`);
+	bot.sendMessage(
+		fromId,
+		`Вы встречаетесь\n
+         ${diffDates(nowDate, happyDate)} дней\n 
+        ${diffHours(nowDate, happyDate)} часов\n 
+        ${diffMinutes(nowDate, happyDate)} минут\n
+        ${diffSeconds(nowDate, happyDate)} секунд`
+	);
+});
